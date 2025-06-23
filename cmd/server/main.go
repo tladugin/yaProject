a9b512cd08885e538429d44558a196aa68c89f15
tladugin <tladugin@yandex.ru>
2025-06-23 10:50:00 +0300

    iter1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,111 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+type gauge struct {
+	Name  string
+	Value float64
+}
+type counter struct {
+	Name  string
+	Value int64
+}
+type MemStorage struct {
+	counterSlice []counter
+	gaugeSlice   []gauge
+}
+
+var globalMemStorage MemStorage
+
+func (s *MemStorage) addGauge(name string, value float64) {
+	for i, m := range s.gaugeSlice {
+		if m.Name == name {
+			s.gaugeSlice[i].Value = value
+			return
+		}
+	}
+
+	s.gaugeSlice = append(s.gaugeSlice, gauge{Name: name, Value: value})
+}
+func (s *MemStorage) addCounter(name string, value int64) {
+	for i, m := range s.counterSlice {
+		if m.Name == name {
+			s.counterSlice[i].Value += value
+			return
+		}
+	}
+
+	s.counterSlice = append(s.counterSlice, counter{Name: name, Value: value})
+}
+
+func handler(res http.ResponseWriter, req *http.Request) {
+
+	if req.Method != http.MethodPost {
+		http.Error(res, "Only POST requests are allowed!", http.StatusNotFound)
+		return
+	}
+
+	parts := strings.Split(req.URL.Path, "/")
+	if len(parts) != 5 {
+		http.Error(res, "Invalid request format", http.StatusNotFound)
+		return
+	}
+
+	if parts[2] == "gauge" {
+		//MemStorage := &MemStorage{counterSlice: make([]counter, 0), gaugeSlice: make([]gauge, 0)}
+
+		partFloat, Error := strconv.ParseFloat(parts[4], 64)
+		if Error != nil {
+			http.Error(res, "Invalid metric value", http.StatusBadRequest)
+		}
+
+		globalMemStorage.addGauge(parts[3], partFloat)
+		res.WriteHeader(http.StatusOK)
+
+		//fmt.Printf("Gauge metrics=================== \n")
+		output := "Gauge metrics:\r\n"
+		for _, element := range globalMemStorage.gaugeSlice {
+			output += fmt.Sprintf("Name: %s, Value: %.1f\n", element.Name, element.Value)
+		}
+		res.Write([]byte(output))
+	} else if parts[2] == "counter" {
+		partInt, Error := strconv.ParseInt(parts[4], 0, 64)
+		if Error != nil {
+			http.Error(res, "Invalid metric value", http.StatusBadRequest)
+		}
+
+		globalMemStorage.addCounter(parts[3], partInt)
+		res.WriteHeader(http.StatusOK)
+		//fmt.Printf("Counter metrics=================== \n")
+		output := "Counter metrics:\r\n"
+		for _, element := range globalMemStorage.counterSlice {
+
+			output += fmt.Sprintf("Name: %s, Value: %d\n", element.Name, element.Value)
+		}
+		res.Write([]byte(output))
+	} else {
+		http.Error(res, "Invalid request format", http.StatusNotFound)
+	}
+	/*else if parts[3] == "" {
+		http.Error(res, "Invalid request path", http.StatusNotFound)
+	} else if parts[2] != "gauge" && parts[2] != "counter" {
+		http.Error(res, "Invalid metric type", http.StatusBadRequest)
+	} else {
+		http.Error(res, "Metrics received!", http.StatusOK)
+	}*/
+}
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/update/`, handler)
+
+	fmt.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		fmt.Printf("Server failed: %v\n", err)
+	}
+
+}

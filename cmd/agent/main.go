fcabe5089841fb91d6fcdc140887a1c1f5982fa3
tladugin <tladugin@yandex.ru>
2025-06-23 16:15:51 +0300

    change error

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"github.com/tladugin/yaProject.git/internal/repository"
+	"runtime"
+)
+
+func main() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+
+	memStorage := repository.NewMemStorage()
+	memStorage.AddGauge("Alloc:", float64(m.Alloc))
+
+	fmt.Println(memStorage.GaugeSlice())
+}

97bcea68852b621d1e6adfce0881cf2800b11eb7
tladugin <tladugin@yandex.ru>
2025-06-25 10:32:53 +0300

    test added

--- internal/handler/handlers.go
+++ internal/handler/handlers.go
@@ -1,7 +1,7 @@
 package handler
 
 import (
-	"fmt"
+	"encoding/json"
 	"github.com/tladugin/yaProject.git/internal/repository"
 	"net/http"
 	"strconv"
@@ -21,12 +21,7 @@ type Server struct {
 }
 
 // функция принимает указатель на структуру Server, что позволяет обрашаться к storage
-func (s *Server) Handler(res http.ResponseWriter, req *http.Request) {
-
-	if req.Method != http.MethodPost {
-		http.Error(res, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
-		return
-	}
+func (s *Server) PostHandler(res http.ResponseWriter, req *http.Request) {
 
 	parts := strings.Split(req.URL.Path, "/")
 	if len(parts) != 5 {
@@ -34,14 +29,8 @@ func (s *Server) Handler(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	if parts[2] != "counter" && parts[2] != "gauge" {
-		http.Error(res, "Invalid request format", http.StatusBadRequest)
-		return
-	}
-
-	if parts[2] == "gauge" {
-		//MemStorage := &MemStorage{counterSlice: make([]counter, 0), gaugeSlice: make([]gauge, 0)}
-
+	switch parts[2] {
+	case "gauge":
 		partFloat, Error := strconv.ParseFloat(parts[4], 64)
 		if Error != nil {
 			http.Error(res, "Invalid metric value", http.StatusBadRequest)
@@ -50,10 +39,7 @@ func (s *Server) Handler(res http.ResponseWriter, req *http.Request) {
 
 		s.storage.AddGauge(parts[3], partFloat)
 		res.WriteHeader(http.StatusOK)
-
-		fmt.Println(s.storage.GaugeSlice())
-
-	} else if parts[2] == "counter" {
+	case "counter":
 		partInt, Error := strconv.ParseInt(parts[4], 0, 64)
 		if Error != nil {
 			http.Error(res, "Invalid metric value", http.StatusBadRequest)
@@ -63,8 +49,40 @@ func (s *Server) Handler(res http.ResponseWriter, req *http.Request) {
 		s.storage.AddCounter(parts[3], partInt)
 		res.WriteHeader(http.StatusOK)
 
-		fmt.Println(s.storage.CounterSlice())
-
+		//fmt.Println(s.storage.CounterSlice())
+		/*default:
+		http.Error(res, "Invalid request format", http.StatusBadRequest)
+		return*/
 	}
 
+}
+func (s *Server) GetHandler(res http.ResponseWriter, req *http.Request) {
+	parts := strings.Split(req.URL.Path, "/")
+	if len(parts) != 4 {
+		http.Error(res, "Invalid request format", http.StatusNotFound)
+		return
+	}
+	switch parts[2] {
+	case "gauge":
+		for i, m := range s.storage.GaugeSlice() {
+			if m.Name == parts[3] {
+				res.Header().Set("Content-Type", "application/json")
+				json.NewEncoder(res).Encode(s.storage.GaugeSlice()[i])
+				res.WriteHeader(http.StatusOK)
+
+			}
+		}
+	case "counter":
+		for i, m := range s.storage.CounterSlice() {
+			if m.Name == parts[3] {
+				res.Header().Set("Content-Type", "application/json")
+				json.NewEncoder(res).Encode(s.storage.GaugeSlice()[i])
+				res.WriteHeader(http.StatusOK)
+
+			}
+		}
+	default:
+		http.Error(res, "Invalid request format", http.StatusBadRequest)
+
+	}
 }

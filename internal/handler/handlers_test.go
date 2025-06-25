93d429a25c7978098243a07a66d82bf123de4edc
tladugin <tladugin@yandex.ru>
2025-06-25 10:18:08 +0300

    change error

---
+++ internal/handler/handlers_test.go
@@ -0,0 +1,78 @@
+package handler
+
+import (
+	"github.com/tladugin/yaProject.git/internal/repository"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestNewServer(t *testing.T) {
+	type args struct {
+		s *repository.MemStorage
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Server
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewServer(tt.args.s); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewServer() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestServer_GetHandler(t *testing.T) {
+	type fields struct {
+		storage *repository.MemStorage
+	}
+	type args struct {
+		res http.ResponseWriter
+		req *http.Request
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Server{
+				storage: tt.fields.storage,
+			}
+			s.GetHandler(tt.args.res, tt.args.req)
+		})
+	}
+}
+
+func TestServer_PostHandler(t *testing.T) {
+	type fields struct {
+		storage *repository.MemStorage
+	}
+	type args struct {
+		res http.ResponseWriter
+		req *http.Request
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Server{
+				storage: tt.fields.storage,
+			}
+			s.PostHandler(tt.args.res, tt.args.req)
+		})
+	}
+}

b9550fe311374ecdba3d7340197913e3e5c24899
tladugin <tladugin@yandex.ru>
2025-06-25 17:24:36 +0300

    iter3 (just task)

--- internal/handler/handlers.go
+++ internal/handler/handlers.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/tladugin/yaProject.git/internal/repository"
 	"net/http"
 	"strconv"
@@ -18,6 +19,34 @@ func NewServer(s *repository.MemStorage) *Server {
 
 type Server struct {
 	storage *repository.MemStorage
+}
+
+func (s *Server) MainPage(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(res, "<html><body><ul>")
+	for m, _ := range s.storage.GaugeSlice() {
+
+		fmt.Fprintf(res, "<li>%s: %v</li>", s.storage.GaugeSlice()[m].Name, s.storage.GaugeSlice()[m].Value)
+		/*
+			res.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(res).Encode(s.storage.GaugeSlice()[m])
+			res.WriteHeader(http.StatusOK)
+
+		*/
+	}
+
+	for m, _ := range s.storage.CounterSlice() {
+
+		fmt.Fprintf(res, "<li>%s: %v</li>", s.storage.CounterSlice()[m].Name, s.storage.CounterSlice()[m].Value)
+		/*
+			res.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(res).Encode(s.storage.CounterSlice()[m])
+			res.WriteHeader(http.StatusOK)
+
+		*/
+
+	}
+	fmt.Fprint(res, "</ul></body></html>")
+
 }
 
 // функция принимает указатель на структуру Server, что позволяет обрашаться к storage
@@ -64,22 +93,33 @@ func (s *Server) GetHandler(res http.ResponseWriter, req *http.Request) {
 	}
 	switch parts[2] {
 	case "gauge":
+		getCheck := false
 		for i, m := range s.storage.GaugeSlice() {
 			if m.Name == parts[3] {
+				getCheck = true
+				res.Header().Set("Content-Type", "application/json")
+				json.NewEncoder(res).Encode(s.storage.GaugeSlice()[i])
+				res.WriteHeader(http.StatusOK)
+
+			}
+
+		}
+		if !getCheck {
+			http.Error(res, "No metric found", http.StatusNotFound)
+		}
+	case "counter":
+		getCheck := false
+		for i, m := range s.storage.CounterSlice() {
+			if m.Name == parts[3] {
+				getCheck = true
 				res.Header().Set("Content-Type", "application/json")
 				json.NewEncoder(res).Encode(s.storage.GaugeSlice()[i])
 				res.WriteHeader(http.StatusOK)
 
 			}
 		}
-	case "counter":
-		for i, m := range s.storage.CounterSlice() {
-			if m.Name == parts[3] {
-				res.Header().Set("Content-Type", "application/json")
-				json.NewEncoder(res).Encode(s.storage.GaugeSlice()[i])
-				res.WriteHeader(http.StatusOK)
-
-			}
+		if !getCheck {
+			http.Error(res, "No metric found", http.StatusNotFound)
 		}
 	default:
 		http.Error(res, "Invalid request format", http.StatusBadRequest)

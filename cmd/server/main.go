c44c4f62e883d462d5102a88a55250b43b5df8c7
tladugin <tladugin@yandex.ru>
2025-06-25 10:32:53 +0300

    test added

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,11 +10,21 @@ import (
 func main() {
 
 	storage := repository.NewMemStorage()
+	/*s := handler.NewServer(storage)
+
+	r := chi.NewRouter()
+	r.Post("/update/", s.PostHandler)
+	r.Get("/value/", s.GetHandler)
+	fmt.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		fmt.Printf("Server failed: %v\n", err)
+	}*/
 
 	s := handler.NewServer(storage)
 
 	mux := http.NewServeMux()
-	mux.HandleFunc(`/update/`, s.Handler)
+	mux.HandleFunc(`/update/`, s.PostHandler)
+	mux.HandleFunc(`/value/`, s.GetHandler)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {})
 	fmt.Println("Starting server on :8080")

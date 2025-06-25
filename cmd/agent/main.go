651807d662c2d7d35c552002631704f9aaccd797
tladugin <tladugin@yandex.ru>
2025-06-25 10:18:08 +0300

    change error

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -61,37 +61,37 @@ func main() {
 		for range ticker.C {
 			// Обновляем метрики здесь
 			fmt.Println("Updating metrics...")
-			storage.AddGauge("Alloc:", float64(m.Alloc))
-			storage.AddGauge("BuckHashSys:", float64(m.BuckHashSys))
-			storage.AddGauge("Frees:", float64(m.Frees))
-			storage.AddGauge("GCCPUFraction:", float64(m.GCCPUFraction))
-			storage.AddGauge("GCSys:", float64(m.GCSys))
-			storage.AddGauge("HeapAlloc:", float64(m.HeapAlloc))
-			storage.AddGauge("HeapIdle:", float64(m.HeapIdle))
-			storage.AddGauge("HeapInuse:", float64(m.HeapInuse))
-			storage.AddGauge("HeapObjects:", float64(m.HeapObjects))
-			storage.AddGauge("HeapReleased:", float64(m.HeapReleased))
-			storage.AddGauge("HeapSys:", float64(m.HeapSys))
-			storage.AddGauge("LastGC:", float64(m.LastGC))
-			storage.AddGauge("Lookups:", float64(m.Lookups))
-			storage.AddGauge("MCacheInuse:", float64(m.MCacheInuse))
-			storage.AddGauge("MCacheSys:", float64(m.MCacheSys))
-			storage.AddGauge("MSpanInuse:", float64(m.MSpanInuse))
-			storage.AddGauge("MSpanSys:", float64(m.MSpanSys))
-			storage.AddGauge("Mallocs:", float64(m.Mallocs))
-			storage.AddGauge("NextGC:", float64(m.NextGC))
-			storage.AddGauge("NumForcedGC:", float64(m.NumForcedGC))
-			storage.AddGauge("NumGC:", float64(m.NumGC))
-			storage.AddGauge("OtherSys:", float64(m.OtherSys))
-			storage.AddGauge("PauseTotalNs:", float64(m.PauseTotalNs))
-			storage.AddGauge("StackInuse:", float64(m.StackInuse))
-			storage.AddGauge("StackSys:", float64(m.StackSys))
-			storage.AddGauge("Sys:", float64(m.Sys))
-			storage.AddGauge("TotalAlloc:", float64(m.TotalAlloc))
+			storage.AddGauge("Alloc", float64(m.Alloc))
+			storage.AddGauge("BuckHashSys", float64(m.BuckHashSys))
+			storage.AddGauge("Frees", float64(m.Frees))
+			storage.AddGauge("GCCPUFraction", float64(m.GCCPUFraction))
+			storage.AddGauge("GCSys", float64(m.GCSys))
+			storage.AddGauge("HeapAlloc", float64(m.HeapAlloc))
+			storage.AddGauge("HeapIdle", float64(m.HeapIdle))
+			storage.AddGauge("HeapInuse", float64(m.HeapInuse))
+			storage.AddGauge("HeapObjects", float64(m.HeapObjects))
+			storage.AddGauge("HeapReleased", float64(m.HeapReleased))
+			storage.AddGauge("HeapSys", float64(m.HeapSys))
+			storage.AddGauge("LastGC", float64(m.LastGC))
+			storage.AddGauge("Lookups", float64(m.Lookups))
+			storage.AddGauge("MCacheInuse", float64(m.MCacheInuse))
+			storage.AddGauge("MCacheSys", float64(m.MCacheSys))
+			storage.AddGauge("MSpanInuse", float64(m.MSpanInuse))
+			storage.AddGauge("MSpanSys", float64(m.MSpanSys))
+			storage.AddGauge("Mallocs", float64(m.Mallocs))
+			storage.AddGauge("NextGC", float64(m.NextGC))
+			storage.AddGauge("NumForcedGC", float64(m.NumForcedGC))
+			storage.AddGauge("NumGC", float64(m.NumGC))
+			storage.AddGauge("OtherSys", float64(m.OtherSys))
+			storage.AddGauge("PauseTotalNs", float64(m.PauseTotalNs))
+			storage.AddGauge("StackInuse", float64(m.StackInuse))
+			storage.AddGauge("StackSys", float64(m.StackSys))
+			storage.AddGauge("Sys", float64(m.Sys))
+			storage.AddGauge("TotalAlloc", float64(m.TotalAlloc))
 
-			storage.AddGauge("RandomValue:", rand.Float64())
+			storage.AddGauge("RandomValue", rand.Float64())
 
-			storage.AddCounter("PollCount:", 1)
+			storage.AddCounter("PollCount", 1)
 
 			// После завершения обновления, возобновляем отправку метрик на сервер
 			resumeCh <- struct{}{}
@@ -108,16 +108,18 @@ func main() {
 			for i := range storage.GaugeSlice() {
 				err := sendMetric("gauge", storage, i)
 				if err != nil {
-					fmt.Println("Error sending metric:", err)
 					fmt.Println(storage.GaugeSlice()[i])
+					fmt.Println("Error sending metric:", err)
+
 				}
 
 			}
 			for i := range storage.CounterSlice() {
 				err := sendMetric("counter", storage, i)
 				if err != nil {
-					fmt.Println("Error sending metric:", err)
 					fmt.Println(storage.CounterSlice()[i])
+					fmt.Println("Error sending metric:", err)
+
 				}
 
 			} // отправляем метрики на сервер

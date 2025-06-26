cffab70e96b9580fe060f4070e2f47d689879877
tladugin <tladugin@yandex.ru>
2025-06-26 10:19:25 +0300

    iter3 (just task)

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
 	"github.com/tladugin/yaProject.git/internal/handler"
 	"github.com/tladugin/yaProject.git/internal/repository"
 	"net/http"
@@ -10,27 +11,38 @@ import (
 func main() {
 
 	storage := repository.NewMemStorage()
-	/*s := handler.NewServer(storage)
+	s := handler.NewServer(storage)
 
 	r := chi.NewRouter()
-	r.Post("/update/", s.PostHandler)
-	r.Get("/value/", s.GetHandler)
+
+	r.Route("/", func(r chi.Router) {
+		r.Get("/", s.MainPage)
+		r.Get("/value/{metric}/{name}", s.GetHandler)
+	})
+	r.Route("/update", func(r chi.Router) {
+
+		r.Post("/{metric}/{name}/{value}", s.PostHandler)
+	})
+	//r.Route("/value", func(r chi.Router) {
+	//	r.Get("/{metric}/{name}", s.GetHandler)
+	//})
 	fmt.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		fmt.Printf("Server failed: %v\n", err)
-	}*/
+	}
 
-	s := handler.NewServer(storage)
+	/*s := handler.NewServer(storage)
 
 	mux := http.NewServeMux()
+
+	mux.HandleFunc(`/`, s.MainPage)
 	mux.HandleFunc(`/update/`, s.PostHandler)
 	mux.HandleFunc(`/value/`, s.GetHandler)
-	mux.HandleFunc(`/`, s.MainPage)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {})
 	fmt.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", mux); err != nil {
 		fmt.Printf("Server failed: %v\n", err)
 	}
-
+	*/
 }
--- internal/handler/handlers.go
+++ internal/handler/handlers.go
@@ -1,12 +1,11 @@
 package handler
 
 import (
-	"encoding/json"
 	"fmt"
+	"github.com/go-chi/chi/v5"
 	"github.com/tladugin/yaProject.git/internal/repository"
 	"net/http"
 	"strconv"
-	"strings"
 )
 
 // создаем функцию NewServer которая возвращает указать на нашу структуру Server, которая содержит storage
@@ -22,64 +21,58 @@ type Server struct {
 }
 
 func (s *Server) MainPage(res http.ResponseWriter, req *http.Request) {
-	if req.Method != "GET" {
-		res.WriteHeader(http.StatusMethodNotAllowed)
-	} else if req.URL.Path != "/" {
-		res.WriteHeader(http.StatusNotFound)
-	} else {
-		fmt.Fprint(res, "<html><body><ul>")
-		for m := range s.storage.GaugeSlice() {
 
-			fmt.Fprintf(res, "<li>%s: %v</li>", s.storage.GaugeSlice()[m].Name, s.storage.GaugeSlice()[m].Value)
+	fmt.Fprint(res, "<html><body><ul>")
+	for m := range s.storage.GaugeSlice() {
 
-			//res.Header().Set("Content-Type", "application/json")
-			//json.NewEncoder(res).Encode(s.storage.GaugeSlice()[m])
-			//res.WriteHeader(http.StatusOK)
-		}
+		fmt.Fprintf(res, "<li>%s: %v</li>", s.storage.GaugeSlice()[m].Name, s.storage.GaugeSlice()[m].Value)
 
-		for m := range s.storage.CounterSlice() {
-
-			fmt.Fprintf(res, "<li>%s: %v</li>", s.storage.CounterSlice()[m].Name, s.storage.CounterSlice()[m].Value)
-
-			//res.Header().Set("Content-Type", "application/json")
-			//json.NewEncoder(res).Encode(s.storage.CounterSlice()[m])
-			//res.WriteHeader(http.StatusOK)
-
-		}
-		fmt.Fprint(res, "</ul></body></html>")
+		//res.Header().Set("Content-Type", "application/json")
+		//json.NewEncoder(res).Encode(s.storage.GaugeSlice()[m])
+		//res.WriteHeader(http.StatusOK)
 	}
+
+	for m := range s.storage.CounterSlice() {
+
+		fmt.Fprintf(res, "<li>%s: %v</li>", s.storage.CounterSlice()[m].Name, s.storage.CounterSlice()[m].Value)
+
+		//res.Header().Set("Content-Type", "application/json")
+		//json.NewEncoder(res).Encode(s.storage.CounterSlice()[m])
+		//res.WriteHeader(http.StatusOK)
+
+	}
+	fmt.Fprint(res, "</ul></body></html>")
 }
 
 // функция принимает указатель на структуру Server, что позволяет обрашаться к storage
 func (s *Server) PostHandler(res http.ResponseWriter, req *http.Request) {
 
-	if req.Method != "POST" {
-		res.WriteHeader(http.StatusMethodNotAllowed)
-	}
-	parts := strings.Split(req.URL.Path, "/")
-	if len(parts) != 5 {
-		http.Error(res, "Invalid request format", http.StatusNotFound)
-		return
-	}
+	//params := chi.URLParam(req, "URL")
+	metric := chi.URLParam(req, "metric")
+	println(metric)
+	name := chi.URLParam(req, "name")
+	println(name)
+	value := chi.URLParam(req, "value")
+	println(value)
 
-	switch parts[2] {
+	switch metric {
 	case "gauge":
-		partFloat, Error := strconv.ParseFloat(parts[4], 64)
+		partFloat, Error := strconv.ParseFloat(value, 64)
 		if Error != nil {
 			http.Error(res, "Invalid metric value", http.StatusBadRequest)
 			return
 		}
 
-		s.storage.AddGauge(parts[3], partFloat)
+		s.storage.AddGauge(name, partFloat)
 		res.WriteHeader(http.StatusOK)
 	case "counter":
-		partInt, Error := strconv.ParseInt(parts[4], 0, 64)
+		partInt, Error := strconv.ParseInt(value, 0, 64)
 		if Error != nil {
 			http.Error(res, "Invalid metric value", http.StatusBadRequest)
 			return
 		}
 
-		s.storage.AddCounter(parts[3], partInt)
+		s.storage.AddCounter(name, partInt)
 		res.WriteHeader(http.StatusOK)
 
 	//fmt.Println(s.storage.CounterSlice())
@@ -90,22 +83,19 @@ func (s *Server) PostHandler(res http.ResponseWriter, req *http.Request) {
 
 }
 func (s *Server) GetHandler(res http.ResponseWriter, req *http.Request) {
-	if req.Method != "GET" {
-		http.Error(res, "Invalid request format", http.StatusBadRequest)
-	}
-	parts := strings.Split(req.URL.Path, "/")
-	if len(parts) != 4 {
-		http.Error(res, "Invalid request format", http.StatusNotFound)
-		return
-	}
-	switch parts[2] {
+
+	metric := chi.URLParam(req, "metric")
+	println(metric)
+	name := chi.URLParam(req, "name")
+	println(name)
+
+	switch metric {
 	case "gauge":
 		getCheck := false
 		for i, m := range s.storage.GaugeSlice() {
-			if m.Name == parts[3] {
+			if m.Name == name {
 				getCheck = true
-				res.Header().Set("Content-Type", "application/json")
-				json.NewEncoder(res).Encode(s.storage.GaugeSlice()[i])
+				fmt.Fprintf(res, "name:%s value:%f", s.storage.GaugeSlice()[i].Name, s.storage.GaugeSlice()[i].Value)
 				res.WriteHeader(http.StatusOK)
 
 			}
@@ -117,10 +107,13 @@ func (s *Server) GetHandler(res http.ResponseWriter, req *http.Request) {
 	case "counter":
 		getCheck := false
 		for i, m := range s.storage.CounterSlice() {
-			if m.Name == parts[3] {
+			if m.Name == name {
 				getCheck = true
-				res.Header().Set("Content-Type", "application/json")
-				json.NewEncoder(res).Encode(s.storage.GaugeSlice()[i])
+				println(m.Name)
+				println(m.Value)
+				//res.Header().Set("Content-Type", "application/json")
+				//json.NewEncoder(res).Encode(s.storage.GaugeSlice()[i])
+				fmt.Fprintf(res, "name:%s value:%d", s.storage.CounterSlice()[i].Name, s.storage.CounterSlice()[i].Value)
 				res.WriteHeader(http.StatusOK)
 
 			}
@@ -128,9 +121,6 @@ func (s *Server) GetHandler(res http.ResponseWriter, req *http.Request) {
 		if !getCheck {
 			http.Error(res, "No metric found", http.StatusNotFound)
 		}
-	default:
-		http.Error(res, "Invalid request format", http.StatusBadRequest)
-		return
 
 	}
 }

dde4cd21e3a34156b64bb6c99a05d0ec1de1a082
tladugin <tladugin@yandex.ru>
2025-06-29 19:23:20 +0300

    iter4 flags added

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -11,7 +11,7 @@ var pollIntervalTime string
 func parseFlags() {
 	// регистрируем переменную flagRunAddr
 	// как аргумент -a со значением :8080 по умолчанию
-	flag.StringVar(&flagRunAddr, "a", "http://localhost:8080", "address and port to run server")
+	flag.StringVar(&flagRunAddr, "a", "localhost:8080", "address and port to run server")
 	flag.StringVar(&reportIntervalTime, "r", "10", "time interval to report")
 	flag.StringVar(&pollIntervalTime, "p", "2", "poll interval")
 	// парсим переданные серверу аргументы в зарегистрированные переменные

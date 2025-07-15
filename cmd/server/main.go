95e3487380d355921e4cc4c83247eda2d5ec82fc
tladugin <tladugin@yandex.ru>
2025-07-15 20:08:32 +0300

    iter6

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,29 +1,42 @@
 package main
 
 import (
-	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/tladugin/yaProject.git/internal/handler"
+	"github.com/tladugin/yaProject.git/internal/logger"
 	"github.com/tladugin/yaProject.git/internal/repository"
+	"go.uber.org/zap"
 	"net/http"
 )
 
+var sugar zap.SugaredLogger
+
 func main() {
+
+	log, err := zap.NewDevelopment()
+	if err != nil {
+		// вызываем панику, если ошибка
+		panic(err)
+	}
+	defer log.Sync()
+
+	sugar = *log.Sugar()
+
 	parseFlags()
+
 	storage := repository.NewMemStorage()
 	s := handler.NewServer(storage)
 
 	r := chi.NewRouter()
 
 	r.Route("/", func(r chi.Router) {
-		r.Get("/", s.MainPage)
-		r.Get("/value/{metric}/{name}", s.GetHandler)
-		r.Post("/update/{metric}/{name}/{value}", s.PostHandler)
+
+		r.Get("/", logger.LoggingAnswer(s.MainPage, sugar))
+		r.Get("/value/{metric}/{name}", logger.LoggingAnswer(s.GetHandler, sugar))
+		r.Post("/update/{metric}/{name}/{value}", logger.LoggingRequest(s.PostHandler, sugar))
 	})
-
-	fmt.Println("Starting server on :", flagRunAddr)
+	sugar.Infoln("Starting server on :", flagRunAddr)
 	if err := http.ListenAndServe(flagRunAddr, r); err != nil {
-		fmt.Printf("Server failed: %v\n", err)
+		sugar.Errorln("Server failed: %v\n", err)
 	}
-
 }
